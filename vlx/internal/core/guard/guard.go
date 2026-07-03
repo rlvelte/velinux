d42d92c3704f0248f850c99eabbef3dae1949c94
Robin L. Velte <robin.leandro.velte@icloud.com>
2026-07-03 21:20:57 +0200

    refactor: simplify guard module

--- vlx/internal/core/guard/guard.go
+++ vlx/internal/core/guard/guard.go
@@ -4,31 +4,12 @@ import (
 	"errors"
 	"fmt"
 	"net"
-	"os"
 	"os/exec"
-	"strings"
 	"time"
 )
 
-const osReleasePath = "/etc/os-release"
-
-// OS enforces that the application runs on supported distributions.
-func OS() error {
-	info, err := os.ReadFile(osReleasePath)
-	if err != nil {
-		return err
-	}
-
-	content := strings.ToLower(string(info))
-	if !strings.Contains(content, "opensuse") {
-		return fmt.Errorf("%s is not an OpenSUSE release", osReleasePath)
-	}
-
-	return nil
-}
-
-// Connection checks for basic internet connectivity.
-func Connection() error {
+// Network checks for basic internet connectivity.
+func Network() error {
 	conn, err := net.DialTimeout("tcp", "1.1.1.1:53", 3*time.Second)
 	if err != nil {
 		return fmt.Errorf("you seem to be offline: %s", err)
@@ -45,5 +26,6 @@ func Binaries(required ...string) error {
 			return errors.New("required binary not found: " + bin)
 		}
 	}
+
 	return nil
 }
