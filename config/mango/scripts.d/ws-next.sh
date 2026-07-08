783ea5bf10a2fc400584b7686884cc67126f4cea
Robin L. Velte <robin.leandro.velte@icloud.com>
2026-07-08 21:09:23 +0200

    refactor: remove mangowm config

--- config/mango/scripts.d/ws-next.sh
+++
@@ -1,4 +0,0 @@
-current=$(mmsg get all-monitors | jq -r '.monitors[] | select(.active == true) | .active_tags[0]')
-if [ "$current" -lt 4 ]; then
-    mmsg dispatch view,$((current + 1))
-fi
--- config/mango/scripts.d/ws-prev.sh
+++
@@ -1,4 +0,0 @@
-current=$(mmsg get all-monitors | jq -r '.monitors[] | select(.active == true) | .active_tags[0]')
-if [ "$current" -gt 1 ]; then
-    mmsg dispatch view,$((current - 1))
-fi
